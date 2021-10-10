99a7e6d9b0e9ebe338187237b27e1d65fb70f692
Vishwanath <vishwanathvishwabai@gmail.com>
2021-10-10 10:45:55 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,22 @@ connectDB();
 
 app.use('/api/v1/upload' , require('./routes/router'));
 
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....')
+    })
+}
+
+
+
 const PORT = process.env.PORT || 5000;
 
 
