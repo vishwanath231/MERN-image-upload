b08fba2c35513e486e6a046c208480d60987a2be
Vishwanath <vishwanathvishwabai@gmail.com>
2021-10-11 09:54:17 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,10 @@ app.use('/api/v1/upload' , require('./routes/router'));
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.use(express.static('/frontend/build'));
 
     app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, '/frontend', 'build', 'index.html'))
     )
 } else {
     app.get('/', (req, res) => {
